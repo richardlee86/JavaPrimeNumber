f99fd7644209d29977a9711eeff1fabf08cf4a27
richardlee86 <richard.t.lee86@gmail.com>
2021-04-29 20:32:53 +0200

    Added Car Class and created "move" method. Added test cases for move method. Project completed to specifications

---
+++ Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public void move(int distance) {
+        for (int i = distance; i > 0; i--) {
+            System.out.println("Moving");
+        }
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
+    // Method to determine if a number is prime as well as
+    // odd or even. It takes one integer argument and returns a string.
     public static String isPrime(int a) {
+
+        /*
+         * For loop that iterates starting with the number 2 and determines if the
+         * modulus of int a and "i" is zero or not. Also checks if a number is odd or
+         * even with a modulo check
+         */
         for (int i = 2; i < a; i++) {
+
+            // Empty string to hold Odd or Even determination message
             String message = "";
 
             if (a % i == 0 && a % 2 == 0) {
@@ -16,10 +26,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Test Cases
+        // Test Cases for Prime Number method
         System.out.println(isPrime(27));
         System.out.println(isPrime(24));
         System.out.println(isPrime(2));
+
+        // Create new instance of Car and call the move method
+        Car car = new Car();
+        car.move(5);
+        car.move(2);
+        car.move(10);
+
     }
 
 }
